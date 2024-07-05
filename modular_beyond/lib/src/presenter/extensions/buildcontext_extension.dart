13e98c3deee87b46a1951f4c5cb4c8618175a20f
Tiago Silva <tsilvasantos38@gmail.com>
2024-07-05 11:33:06 -0300

    fix: fix condition

--- modular_beyond/lib/src/presenter/extensions/buildcontext_extension.dart
+++ modular_beyond/lib/src/presenter/extensions/buildcontext_extension.dart
@@ -11,7 +11,8 @@ void popCurrentContext() {
 
 extension BuildcontextExtension on BuildContext {
   void setCurrentContext() {
-    if (_contextStack.lastOrNull?.widget.hashCode == widget.hashCode) {
+    if (_contextStack.isNotEmpty &&
+        _contextStack.last.toString() == toString()) {
       return;
     }
     _contextStack.add(this);
